fdccda46d29b176edca07317a15e54d4ccbfbe84
Taciano Morais Silva <tacianosilva@gmail.com>
2021-12-07 16:05:28 -0300

    infra: add documentação OpenAPI com Swagger #9

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,8 +1,11 @@
+import { Get, Route } from "tsoa";
+
 interface PingResponse {
     message: string;
 }
-
+@Route("ping")
 export default class PingController {
+    @Get("/")
     public async getMessage(): Promise<PingResponse> {
         return {
             message: "pong",
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 import Router from "./routes";
 
 const PORT = process.env.PORT || 8000;
@@ -9,6 +10,16 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 
 app.use(Router);
 
