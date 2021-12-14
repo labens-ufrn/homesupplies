63507b55689f76ac040372c9ae0531c26147c096
Taciano Morais Silva <tacianosilva@gmail.com>
2021-12-14 11:36:20 -0300

    feat: Criação do Controlador de User #11

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { Get, Route, Tags, Post, Body, Path } from "tsoa";
+import { User } from "../models";
+import { UserService, IUserPayload } from "../services/user";
+
+@Route("users")
+@Tags("User")
+export default class UserController {
+
+  service = new UserService();
+
+  @Get("/")
+  public async getUsers(): Promise<Array<User>> {
+    return this.service.getUsers();
+  }
+
+  @Post("/")
+  public async createUser(@Body() body: IUserPayload): Promise<User> {
+    return this.service.createUser(body);
+  }
+
+  @Get("/:id")
+  public async getUser(@Path() id: string): Promise<User | null> {
+    return this.service.getUser(Number(id));
+  }
+}
